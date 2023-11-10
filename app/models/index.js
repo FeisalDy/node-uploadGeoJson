b56bf576c11850e31077aed50cdf436d9260e51a
feisaldy <feisaldy26@gmail.com>
2023-11-10 15:04:37 +0700

    add docker to app

--- app/models/index.js
+++ app/models/index.js
@@ -6,7 +6,6 @@ mongoose.Promise = global.Promise
 const db = {}
 db.mongoose = mongoose
 db.url = dbConfig.url
-// db.tutorials = require('./tutorial.model.js')(mongoose)
 db.geojson = require('./geojson.model.js')(mongoose)
 db.geokota = require('./geokota.model.js')(mongoose)
 
